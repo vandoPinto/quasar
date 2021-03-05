3520da182f231fa095e6978e3eca2d4102abc3f0
vandoPinto <vandopintoo@gmail.com>
2021-03-05 14:31:27 -0300

    Add files via upload

---
+++ src/layouts/MainLayout.vue
@@ -0,0 +1,152 @@
+<template>
+  <q-layout view="lHh Lpr lFf">
+    <q-header>
+      <q-toolbar>
+        <q-btn
+          flat
+          dense
+          round
+          icon="menu"
+          aria-label="Menu"
+          @click="leftDrawerOpen = !leftDrawerOpen"
+        />
+      </q-toolbar>
+      <div class="q-px-lg q-pt-xl q-mb-md">
+        <div class="text-h3">Todo</div>
+        <div class="text-subtitle1">{{todaysDate}}</div>
+      </div>
+      <q-img
+      src="https://cdn.pixabay.com/photo/2016/08/09/21/54/lake-1581879_960_720.jpg"
+      class="header-image absolute-top" />
+    </q-header>
+
+   <q-drawer
+        v-model="leftDrawerOpen"
+        show-if-above
+        :width="250"
+        :breakpoint="600"
+      >
+        <q-scroll-area style="height: calc(100% - 192px); margin-top: 192px; border-right: 1px solid #ddd">
+          <q-list padding>
+            <q-item
+              to="/"
+              clickable
+              v-ripple>
+              <q-item-section avatar>
+                <q-icon name="list" />
+              </q-item-section>
+
+              <q-item-section>
+                Todo
+              </q-item-section>
+            </q-item>
+
+            <q-item
+              to="/help"
+              clickable
+              v-ripple>
+              <q-item-section avatar>
+                <q-icon name="help" />
+              </q-item-section>
+
+              <q-item-section>
+                Help
+              </q-item-section>
+            </q-item>
+
+          </q-list>
+        </q-scroll-area>
+
+        <q-img class="absolute-top" src="https://cdn.pixabay.com/photo/2016/08/09/21/54/lake-1581879_960_720.jpg" style="height: 192px">
+          <div class="absolute-bottom bg-transparent">
+            <q-avatar size="56px" class="q-mb-sm">
+              <img src="https://lh3.googleusercontent.com/a-/AOh14Giau7Rt_0UKflYAlcbqddxoi9WG8DQ1OEZbfcdmrMM=s96-c-rg-br100">
+            </q-avatar>
+            <div class="text-weight-bold">Vando Pinto</div>
+            <div>in/vandopinto</div>
+          </div>
+        </q-img>
+      </q-drawer>
+
+    <q-page-container>
+      <keep-alive>
+        <router-view />
+      </keep-alive>
+    </q-page-container>
+  </q-layout>
+</template>
+
+<script>
+import { date } from 'quasar'
+// import EssentialLink from 'components/EssentialLink.vue'
+
+const linksData = [
+  {
+    title: 'Docs',
+    caption: 'quasar.dev',
+    icon: 'school',
+    link: 'https://quasar.dev'
+  },
+  {
+    title: 'Github',
+    caption: 'github.com/quasarframework',
+    icon: 'code',
+    link: 'https://github.com/quasarframework'
+  },
+  {
+    title: 'Discord Chat Channel',
+    caption: 'chat.quasar.dev',
+    icon: 'chat',
+    link: 'https://chat.quasar.dev'
+  },
+  {
+    title: 'Forum',
+    caption: 'forum.quasar.dev',
+    icon: 'record_voice_over',
+    link: 'https://forum.quasar.dev'
+  },
+  {
+    title: 'Twitter',
+    caption: '@quasarframework',
+    icon: 'rss_feed',
+    link: 'https://twitter.quasar.dev'
+  },
+  {
+    title: 'Facebook',
+    caption: '@QuasarFramework',
+    icon: 'public',
+    link: 'https://facebook.quasar.dev'
+  },
+  {
+    title: 'Quasar Awesome',
+    caption: 'Community Quasar projects',
+    icon: 'favorite',
+    link: 'https://awesome.quasar.dev'
+  }
+]
+
+export default {
+  name: 'MainLayout',
+  // components: { EssentialLink },
+  data () {
+    return {
+      leftDrawerOpen: false,
+      essentialLinks: linksData
+    }
+  },
+  computed: {
+    todaysDate () {
+      return date.formatDate(Date.now(), 'dddd D MMMM')
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+  .header-image{
+    height: 100%;
+    z-index: -1;
+    opacity:0.2;
+    filter: grayscale(100%);
+  }
+</style>
---
+++ src/pages/Help.vue
@@ -0,0 +1,7 @@
+<template>
+  <q-page class="q-pa-lg">
+   <h5 class="q-mt-none">Help</h5>
+   <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Aliquid sequi corporis placeat! Dolore blanditiis dolorum tempore reprehenderit? Recusandae iure quia illo neque dignissimos blanditiis fuga. Tempore ad soluta temporibus blanditiis.</p>
+   <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos perspiciatis ab totam quaerat non, voluptas minima eum sunt aut? Doloribus, expedita? Et impedit in debitis vel cum adipisci, esse voluptates.</p>
+  </q-page>
+</template>
